c43412b5f3b88888a127dd364a8834fbffc80237
Keisuke Ichida <ichida@ube-k.ac.jp>
2021-01-10 14:30:28 +0900

    Add a test script

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -22,13 +22,13 @@ class WallTraceTest(unittest.TestCase):
         self.assertTrue(left == right == 0,"can't stop")
 
         left, right = self.set_and_get(0,5,1000,0) #side direction is not a trigger of stop
-        self.assertTrue(left == right != 0 ,"stop wrongly by side sensors")
+        self.assertTrue(left == right != 0,"stop wrongly by side sensors")
 
         left, right = self.set_and_get(0,10,0,0) #curve to left
-        self.assertTrue(left < right, "don't curve to left")
+        self.assertTrue(left < right,"don't curve to left")
 
         left, right = self.set_and_get(0,200,0,0) #curve to right
-        self.assertTrue(left > right, "don't curve to right")
+        self.assertTrue(left > right,"don't curve to right")
 
         left, right = self.set_and_get(0,5,0,0) #don't control when far from a wall
         self.assertTrue(0 < left == right, "curve wrongly")
