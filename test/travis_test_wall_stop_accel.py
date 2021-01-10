6c6b09a4c984691d6c3e5a2255ac113d7b14acda
Keisuke Ichida <ichida@ube-k.ac.jp>
2021-01-10 17:09:54 +0900

    Add a test script

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -12,24 +12,24 @@ class WallStopAccelTest(unittest.TestCase):
 
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
-            left = int(float(lf.readline().rstrip()))
-            right = int(float(rf.readline().rstrip()))
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
 
         return left, right
 
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) #total: 600
-        self.assertTrue(left == right == 0,"can't stop")
+        self.assertTrue(left == 0 and right == 0,"can't stop")
 
         left, right = self.set_and_get(40,0,0,9) #total: 49
-        self.assertTrue(0 < left == right < 1000,"can't move again")
+        self.assertTrue(left > 0 and right < 1000,"can't move again")
 
         time.sleep(5.0)
         left, right = self.set_and_get(40,0,0,9) #total: 49
-        self.assertTrue(2000 < left == right, "can't accerelate")
+        self.assertTrue(left > 2000 and right > 2000, "can't accerelate")
 
         left, right = self.set_and_get(15,0,20,15) #total: 50
-        self.assertTrue(left == right == 0,"can't stop again")
+        self.assertTrue(left == 0 and right == 0,"can't stop again")
 
 if __name__ == '__main__':
     time.sleep(3)
